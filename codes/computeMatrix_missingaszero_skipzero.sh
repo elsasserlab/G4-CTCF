483a48bb7916a50c36dd709abe7ca0f1571c52f7
heteyszabolcs <heteysz@gmail.com>
2024-09-03 15:11:38 +0200

    tidying of plot_figures.Rmd

--- codes/computeMatrix_missingaszero_skipzero.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-cd $dir
-
-computeMatrix reference-point -o $output.matrix.gz --outFileNameMatrix $output.matrix.tab --outFileSortedRegions $output.regions.bed -S $scores -R $region -b $bef -a $bef --samplesLabel $samplesLabel --referencePoint center -bs $binSize --missingDataAsZero --skipZeros --blackListFileName $blacklist -p 5
--- codes/computeMatrix_missingaszero_skipzero_threshATACseq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# a maximum threshold is used to remove effect of 1% outliers in the ATAC-seq signal
-
-cd $dir
-
-computeMatrix reference-point -o $output.matrix.gz --outFileNameMatrix $output.matrix.tab --outFileSortedRegions $output.regions.bed -S $scores -R $region -b $bef -a $bef --samplesLabel $samplesLabel --referencePoint center -bs $binSize --missingDataAsZero --skipZeros --blackListFileName $blacklist -p 5 ----maxThreshold 1000
